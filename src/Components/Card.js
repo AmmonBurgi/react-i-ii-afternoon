35a8ba2ed18b746a33ffeb3817c5275b24039e5d
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-02-28 22:07:03 -0700

    completed project

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,16 +4,23 @@ function Card(props){
     return(
         <div className='outerDiv'>
         <div className='card'>
-            <div className='id'>{props.theData[props.Num].id}/25</div>
-            <div className='title'>{props.theData[props.Num].name.first} {props.theData[props.Num].name.last}</div>
-            <div className='from'>From: {props.theData[props.Num].city}, {props.theData[props.Num].country}</div>
-            <div className='job'>Title: {props.theData[props.Num].title}</div>
-            <div className='employer'>Employer: {props.theData[props.Num].employer}</div>
+            <div className='id'>
+                <p className='pTag'>{props.theData[props.Num].name.first} {props.theData[props.Num].name.last}</p> 
+            {props.theData[props.Num].id}/25 
+            </div>
+            <div className='from'>
+                <p className='bold'>From:</p>
+                {props.theData[props.Num].city}, {props.theData[props.Num].country}</div>
+            <div className='job'>
+                <p className='bold'>Job Title: </p>  {props.theData[props.Num].title}</div>
+            <div className='employer'>
+                <p className='bold'>Employer:</p> {props.theData[props.Num].employer}</div>
             {/* <div className='FavMovie'>Favorite Movies:</div> */}
-            <ol>Favorite Movies:
-                <li>{props.theData[props.Num].favoriteMovies[0]}</li>
-                <li>{props.theData[props.Num].favoriteMovies[1]}</li>
-                <li>{props.theData[props.Num].favoriteMovies[2]}</li>
+            <ol className='ol'>
+                <p className='bold' className='favMovie'>Favorite Movies:</p>
+                <li className='top-list'>1. {props.theData[props.Num].favoriteMovies[0]}</li>
+                <li className='li'>2. {props.theData[props.Num].favoriteMovies[1]}</li>
+                <li className='li'>3. {props.theData[props.Num].favoriteMovies[2]}</li>
             </ol>
         </div>
         </div>
--- src/Components/Clicker.js
+++ src/Components/Clicker.js
@@ -3,13 +3,14 @@ import React from 'react'
 function Clicker(props){
     return(
         <div className='alignClick'>
-        <button className='Previous' onClick={props.clickPrev}>&#10094; Previous</button>
+            
+        <button className='Previous' onClick={props.clickPrev}>{ '<Previous' }</button>
         <div>
         <button className='edit'>Edit</button>
         <button className='delete'>Delete</button>
         <button className='new'>New</button>
         </div>
-        <button className='Next' onClick={props.clickNext}>Next</button>
+        <button className='Next' onClick={props.clickNext}>{'Next>'}</button>
     </div>
     )
 }
